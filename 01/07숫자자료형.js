04ac9c3ae4fe1f3291289dd9693b2b5effd03d10
cswwe <frontendcsw@gmail.com>
2023-01-12 18:02:06 +0900

    commit

--- 01/07숫자자료형.js
+++ 01/07숫자자료형.js
@@ -1,6 +1,7 @@
 let a = 100; //NaN Not a Number 숫자가 아님
-a = a + 10; //110
-document.write(`${a}<br>`);
-a = a / 10; //11
-document.write(`${a}<br>`);
+a = a + 10; //  a 재선언 현재의a= 이전의a(100) + 10 이므로= 110
+document.write(`${a}<br>`); //110 출력
+a = a / 10; // 현재의 a= 이전의a(110)/10이므로 = 11
+document.write(`${a}<br>`); //11출력
 document.write(`a-10=${a - 10}<br>a*10=${a*10}`);
+              //a-10=1 출력  <줄바꿈>  a*10=110 출력
--- 01/08논리자료형.js
+++ 01/08논리자료형.js
@@ -1,7 +1,10 @@
 /* 논리자료형(참,거짓)  */
 let a=true;
-//a="true";
+// a="true";
 a=false;
-alert(a)
+alert(a) //alert - 팝업창 알림창 / 마지막으로 재선언한 false 가 알림창으로 출력됨
 //자료형을 숫자로 바꿔주는 함수 Number()
-document.write(Number(a))
+document.write(Number(a)) 
+//true 선언시 웹상에 1 출력  
+//false 선언시 웹상에 0으로 출력
+//문자열"" 선언시 웹상에 NaN 출력
